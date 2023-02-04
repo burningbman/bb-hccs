cab6d7a528478e309220483d6db35e643e43fb10
DSONE\OB8 <blake.badders@gmail.com>
2023-02-03 19:45:00 -0500

    Total overhaul to use libram

--- src/ascend.ts
+++ src/ascend.ts
@@ -1,44 +1,25 @@
-import { containsText, print, pvpAttacksLeft, visitUrl } from "kolmafia";
+import { abort, getPermedSkills, print, pvpAttacksLeft, Skill } from "kolmafia";
+import { ascend, $path, $class, Lifestyle, $item, have } from "libram";
+
+function createPermOptions(): { permSkills: Map<Skill, Lifestyle>; neverAbort: boolean } {
+  return {
+    permSkills: new Map(
+      Skill.all()
+        .filter(
+          (skill) => have(skill) && skill.permable && getPermedSkills()[skill.name] === undefined
+        )
+        .map((skill) => [skill, Lifestyle.hardcore])
+    ),
+    neverAbort: false,
+  };
+}
 
 if (pvpAttacksLeft() > 0) {
   print("Run hccs_pre first, dingus.", "red");
+  abort();
 } else {
-  // use once there is a check for not perming skills that are available
-  // ascend(
-  //   Paths.CommunityService,
-  //   $class`Pastamancer`,
-  //   Lifestyle.softcore,
-  //   "blender",
-  //   $item`astral six-pack`,
-  //   $item`astral trousers`
-  // );
-
-  if (!containsText(visitUrl("charpane.php"), "Astral Spirit")) {
-    visitUrl("ascend.php?action=ascend&confirm=on&confirm2=on");
-  }
-  if (!containsText(visitUrl("charpane.php"), "Astral Spirit"))
-    throw "Failed to ascend.";
-  visitUrl("afterlife.php?action=pearlygates");
-
-  if (
-    !visitUrl("afterlife.php?place=permery").includes(
-      "There's nothing we can do for you here"
-    )
-  ) {
-    print(
-      "It seems like there are some unclaimed skills. Deal with that.",
-      "red"
-    );
-  } else {
-    // Pilsners
-    visitUrl("afterlife.php?action=buydeli&whichitem=5046");
-    // Trousers
-    visitUrl("afterlife.php?action=buyarmory&whichitem=5035");
-
-    // Blender, Male, Pastamancer, CS, Normal
-    visitUrl(
-      "afterlife.php?action=ascend&confirmascend=1&whichsign=8&gender=1&whichclass=3&whichpath=25&asctype=2&nopetok=1&noskillsok=1&pwd",
-      true
-    );
-  }
+  ascend($path`Community Service`, $class`Pastamancer`, Lifestyle.normal, 'blender', $item`astral six-pack`, $item`astral trousers`, {
+    permSkills: createPermOptions().permSkills,
+    neverAbort: false
+  });
 }
